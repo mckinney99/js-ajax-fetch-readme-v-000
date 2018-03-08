45ff1f115a2350bc73eaf1c6e771ffc7923b0a77
Eric McKinney <mckinney99@gmail.com>
2018-03-08 20:16:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ fetch('https://api.github.com/repos/jquery/jquery/commits')
     then(res => res.json()).
     then(json => console.log(json))
 
-
+    const token = 'YOUR_TOKEN_HERE'
+    fetch('https://api.github.com/user/repos', {
+      headers: {
+        Authorization: `token ${token}`
+      }
+    }).then(res => res.json()).then(json => console.log(json));
 
 
 /* f695a54f1ddfcce7781284ef11c06a1b3c08fef9
