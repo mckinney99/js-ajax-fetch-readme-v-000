600e9ec6e240a6f921a14d7506ff4c50087fd89b
Eric McKinney <mckinney99@gmail.com>
2018-03-08 20:15:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ fetch('https://api.github.com/repos/jquery/jquery/commits')
   .then(res => res.json())
   .then(json => console.log(json));
 
+  fetch('https://api.github.com/user/repos').
+    then(res => res.json()).
+    then(json => console.log(json))
+
 
 
 
